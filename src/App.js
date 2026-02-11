a3dc73552d5d045657d8452cae885015f9d21f8f
AliMaher <ali.maher0013@gmail.com>
2026-02-11 15:31:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useRef, useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const inputRef = useRef();
+  const handleAddButton = () => {
+    const text = inputRef.current.value;
+    const newItem = { completed: false, text };
+    setTodos([...todos, newItem]);
+    inputRef.current.value = "";
+  };
+
+  const handleItemDone = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+  const handleDeleteItem = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+  console.log(todos);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>ToDo List</h2>
+        <div className="todos-container">
+          <ul>
+            {todos.map(({ text, completed }, index) => {
+              return (
+                <div className="item">
+                  <li
+                    key={index}
+                    className={completed ? "done" : ""}
+                    onClick={() => handleItemDone(index)}
+                  >
+                    {text}
+                  </li>
+                  <span
+                    className="delete"
+                    onClick={() => handleDeleteItem(index)}
+                  >
+                    ❌
+                  </span>
+                </div>
+              );
+            })}
+          </ul>
+        </div>
+        <input ref={inputRef} placeholder="Enter Item..." />
+        <button onClick={handleAddButton}>Add</button>
+      </div>
     </div>
   );
 }
